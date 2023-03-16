e0e5119ba977508419793215f3329a32830a1955
chapcs <benchapman003@gmail.com>
2023-03-16 08:31:46 -0400

    Add project files.

---
+++ HasASecret.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secret
+{
+    class HasASecret
+    {
+        // Does the private keyword make this field secure? No
+        private string secret = "xyzzy";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Secret;
+using System.Reflection;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        HasASecret keeper = new HasASecret();
+
+        // The statement below causes a console error because the field is inaccessible due to being private
+        //Console.WriteLine(keeper.secret);
+
+        // You can still use reflection to get the value of the private field
+        FieldInfo[] fields = keeper.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+        // This loop will cause the string to be printed to console
+        foreach (FieldInfo field in fields)
+        {
+            Console.WriteLine(field.GetValue(keeper));
+        }
+    }
+}
+
+class HasASecret
+{
+    // Does the private keyword make this field secure? No
+    private string secret = "xyzzy";
+}
